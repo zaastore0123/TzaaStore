f982381a4bdc6b12be0666641dd1b9bf18c3509a
zaastore0123 <baayouu2@gmail.com>
2025-08-11 15:16:01 +0700

    products-gag.js

---
+++ js/products-gag.js
@@ -0,0 +1,4 @@
+const products = [
+  { img: "img/c.jpg", title: "Produk C", desc: "Deskripsi C", price: "Rp 30.000", link: "https://wa.me/628813908290?text=Halo+saya+ingin+beli+Produk+C" },
+  { img: "img/d.jpg", title: "Produk D", desc: "Deskripsi D", price: "Rp 35.000", link: "https://wa.me/628813908290?text=Halo+saya+ingin+beli+Produk+D" }
+];
